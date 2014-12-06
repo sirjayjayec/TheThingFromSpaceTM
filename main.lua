c658ae40ea7e35b7b8a7321ef9ac95f2b407acad
sirjayjayec <jackelliott7796@gmail.com>
2014-12-06 18:02:13 +0000

    Configuration settings
    Basic config settings

--- main.lua
+++ main.lua
@@ -1,5 +1,11 @@
+function love.conf(t)
+  t.window.width = 1280
+  t.window.height = 720
+  t.window.title = "The Thing From Space ^ TM"        -- The window title (string)
+  t.window.vsync = false
+end
 function love.load()
-	love.graphics.setNewFont(12)
+  love.graphics.setNewFont(12)
 	love.graphics.setColor(255,0,0)
 	love.graphics.setBackgroundColor(255,255,255)
 end
