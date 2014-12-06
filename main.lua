c31e7ae232699c48978a2c46feb982332e1a1f91
sirjayjayec <jackelliott7796@gmail.com>
2014-12-06 18:11:56 +0000

    Timing error

--- main.lua
+++ main.lua
@@ -4,14 +4,14 @@ function love.load()
 	love.graphics.setBackgroundColor(255,255,255)
 end
 dtotal = 0
+Text = 0
 function love.update(dt)
   posX = 560
   posY = 100
-  Text = dt
   dtotal = dtotal + dt
-  if dtotal >= 0.016666667 then --Run physics at 60fps
-    dtotal = dtotal - 0.016666667
-    --Run world update function
+  if dtotal >= 0.01667 then --Run physics at 60fps
+    Text = tostring(dtotal)
+    dtotal = dtotal - 0.01667 
   end
 end
 function love.draw()
