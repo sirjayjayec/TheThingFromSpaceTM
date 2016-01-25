f567c0e6f1b366782f5a4e2c187d150f565365d4
sirjayjayec <jackelliott7796@gmail.com>
2016-01-25 22:58:00 +0000

    Changed Resolution
    config file updated

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
-  t.window.width = 1920
-  t.window.height = 1080
+  t.window.width = 1280
+  t.window.height = 720
   t.window.title = "The Thing From Space ^ TM"        -- The window title (string)
   t.window.vsync = false
 end
