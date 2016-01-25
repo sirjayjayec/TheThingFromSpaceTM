d240eae521e171e25ece016aad9ba6d6b28a8423
sirjayjayec <jackelliott7796@gmail.com>
2016-01-25 23:43:44 +0000

    FINALLY FIXED
    FUCK SAKE

--- main.lua
+++ main.lua
@@ -242,9 +242,7 @@ function ObjRender()
     elseif ActiveColShape == "square" then
       love.graphics.rectangle("line",ActiveXCord,ActiveYCord,ActiveColSize,ActiveColSize)
     end
-    --if ActiveSprite =! null then 
-    --  love.graphics.draw(ActiveSprite,ActiveXCord,ActiveYCord)
-    ----renderInt=renderInt+1
+    renderInt=renderInt+1
   end
   renderInt = 0
 end
