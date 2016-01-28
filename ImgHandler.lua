6216360a8988ad2acbf01fb46fc7d6dc2b445f2d
sirjayjayec <jackelliott7796@gmail.com>
2016-01-28 04:52:16 +0000

    Fixed crashes left from previous commit

--- ImgHandler.lua
+++ ImgHandler.lua
@@ -7,15 +7,16 @@ function IH.loadTileset(pathToFile, amount)
   tilesetImg = love.graphics.newImage("images/tilesets/" .. pathToFile)
   tilesetImg:setFilter("nearest", "linear")
   images = {}
+  amountOfImages = 10
   y = 0
   while y < amountOfImages do
     x = 0
     while x < amountOfImages do
-      x+=1
+      x = x + 1
       img = love.graphics.newQuad(x * 32, y * 32, 32, 32, tilesetImg:getWidth(), tilesetImg:getHeight())
       table.insert(images, img)
     end
-    y+=1
+    y = y +1
   end
 end
 
